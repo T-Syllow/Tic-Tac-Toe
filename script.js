b6c14cf45efa473085cf472dca812a86a251153d
Tommy Syllow <tommysyllow@gmail.com>
2023-01-16 18:26:49 +0100

    added span into fields for winner decision

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 let activePlayer = ["circle", "cross"];
 let activePlayerIndex = 0; //circle begins..
+let fields = [
+
+    //first-Row
+    [   document.getElementById('field1'),  
+        document.getElementById('field2'),
+        document.getElementById('field3')   ],
+
+    //second-Row
+    [   document.getElementById('field4'),
+        document.getElementById('field5'),
+        document.getElementById('field6')   ],
+
+    //third-Row
+    [   document.getElementById('field7'),
+        document.getElementById('field8'),
+        document.getElementById('field9')   ]
+]
 
 function setMarker(fieldNum) {
     if(activePlayerIndex == 0) {
@@ -14,6 +31,8 @@ function setMarker(fieldNum) {
 }
 
 function checkWinner() {
-    
+    if(fields[0][1] && fields[0],[2] && fields[0],[3]) {
+
+    }
 }
 
