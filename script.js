4f49a2d5e227e3147447fce83f6e8a1b20184383
Tommy Syllow <tommysyllow@gmail.com>
2023-01-17 13:30:50 +0100

    game finished

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let activePlayer = ["o", "x"];
 let activePlayerIndex = 0; //circle begins..
 let fieldValues = [];
+let markedFields = 0;
 
 function render() {
+    fieldValues = [];
     fieldValues.push([document.getElementById('value1'),  
     document.getElementById('value2'),
     document.getElementById('value3')]);
@@ -15,6 +17,7 @@ function render() {
     document.getElementById('value8'),
     document.getElementById('value9')]);
 
+    
     clearFieldMarkers();
     
 }
@@ -32,11 +35,13 @@ function setMarker(fieldNum) {
         if(activePlayerIndex == 0) {
             document.getElementById('circle'+fieldNum).classList.remove('d-none');
             document.getElementById('value'+fieldNum).innerText = 'o';
+            markedFields++;
             checkWinner();
             activePlayerIndex = 1;
         }else if(activePlayerIndex == 1) {
             document.getElementById('cross'+fieldNum).classList.remove('d-none');
             document.getElementById('value'+fieldNum).innerText = 'x';
+            markedFields++;
             checkWinner();
             activePlayerIndex = 0;
         }
@@ -46,53 +51,85 @@ function setMarker(fieldNum) {
 function checkWinner() {
     //horizontal wins. -->
     if(fieldValues[0][0].innerText.includes(activePlayer[activePlayerIndex]) && fieldValues[0][1].innerText.includes(activePlayer[activePlayerIndex]) && fieldValues[0][2].innerText.includes(activePlayer[activePlayerIndex])) {
-        console.log(activePlayer[activePlayerIndex] + " wins!");
+        console.log('a');
         showEndScreen(activePlayer[activePlayerIndex]);
     }
 
-    if(fieldValues[1][0].innerText == activePlayer[activePlayerIndex] && fieldValues[1][1].innerText == activePlayer[activePlayerIndex] && fieldValues[1][2].innerText == activePlayer[activePlayerIndex]) {
-        console.log(activePlayer[activePlayerIndex] + " wins!");
+    else if(fieldValues[1][0].innerText == activePlayer[activePlayerIndex] && fieldValues[1][1].innerText == activePlayer[activePlayerIndex] && fieldValues[1][2].innerText == activePlayer[activePlayerIndex]) {
+        console.log('b');
         showEndScreen(activePlayer[activePlayerIndex]);
     }
 
-    if(fieldValues[2][0].innerText == activePlayer[activePlayerIndex] && fieldValues[2][1].innerText == activePlayer[activePlayerIndex] && fieldValues[2][2].innerText == activePlayer[activePlayerIndex]) {
-        console.log(activePlayer[activePlayerIndex] + " wins!");
+    else if(fieldValues[2][0].innerText == activePlayer[activePlayerIndex] && fieldValues[2][1].innerText == activePlayer[activePlayerIndex] && fieldValues[2][2].innerText == activePlayer[activePlayerIndex]) {
+        console.log('c');
         showEndScreen(activePlayer[activePlayerIndex]);
     }
 
     //vertikal wins. -->
 
-    if(fieldValues[0][0].innerText == activePlayer[activePlayerIndex] && fieldValues[1][0].innerText == activePlayer[activePlayerIndex] && fieldValues[1][0].innerText == activePlayer[activePlayerIndex]) {
-        console.log(activePlayer[activePlayerIndex] + " wins!");
+   else if(fieldValues[0][0].innerText == activePlayer[activePlayerIndex] && fieldValues[1][0].innerText == activePlayer[activePlayerIndex] && fieldValues[2][0].innerText == activePlayer[activePlayerIndex]) {
+        console.log('d');
         showEndScreen(activePlayer[activePlayerIndex]);
     }
 
-    if(fieldValues[0][1].innerText == activePlayer[activePlayerIndex] && fieldValues[1][1].innerText == activePlayer[activePlayerIndex] && fieldValues[2][1].innerText == activePlayer[activePlayerIndex]) {
-        console.log(activePlayer[activePlayerIndex] + " wins!");
+    else if(fieldValues[0][1].innerText == activePlayer[activePlayerIndex] && fieldValues[1][1].innerText == activePlayer[activePlayerIndex] && fieldValues[2][1].innerText == activePlayer[activePlayerIndex]) {
+        console.log('e');
         showEndScreen(activePlayer[activePlayerIndex]);
     }
 
-    if(fieldValues[0][2].innerText == activePlayer[activePlayerIndex] && fieldValues[1][2].innerText == activePlayer[activePlayerIndex] && fieldValues[2][2].innerText == activePlayer[activePlayerIndex]) {
-        console.log(activePlayer[activePlayerIndex] + " wins!");
+    else if(fieldValues[0][2].innerText == activePlayer[activePlayerIndex] && fieldValues[1][2].innerText == activePlayer[activePlayerIndex] && fieldValues[2][2].innerText == activePlayer[activePlayerIndex]) {
+        console.log('f');
         showEndScreen(activePlayer[activePlayerIndex]);
     }
 
     //diagonal wins -->
 
-    if(fieldValues[0][0].innerText == activePlayer[activePlayerIndex] && fieldValues[1][1].innerText == activePlayer[activePlayerIndex] && fieldValues[2][2].innerText == activePlayer[activePlayerIndex]) {
-        console.log(activePlayer[activePlayerIndex] + " wins!");
+    else if(fieldValues[0][0].innerText == activePlayer[activePlayerIndex] && fieldValues[1][1].innerText == activePlayer[activePlayerIndex] && fieldValues[2][2].innerText == activePlayer[activePlayerIndex]) {
+        console.log('g');
         showEndScreen(activePlayer[activePlayerIndex]);
     }
 
-    if(fieldValues[2][0].innerText == activePlayer[activePlayerIndex] && fieldValues[1][1].innerText == activePlayer[activePlayerIndex] && fieldValues[0][2].innerText == activePlayer[activePlayerIndex]) {
-        console.log(activePlayer[activePlayerIndex] + " wins!");
+    else if(fieldValues[2][0].innerText == activePlayer[activePlayerIndex] && fieldValues[1][1].innerText == activePlayer[activePlayerIndex] && fieldValues[0][2].innerText == activePlayer[activePlayerIndex]) {
+        console.log('h');
         showEndScreen(activePlayer[activePlayerIndex]);
     }
+
+    else if(isTie()){
+        showTieScreen();
+    }
+}
+
+function isTie() {
+    if(markedFields >= 9) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+
+    /*let isGameFinished = false;
+    for (let i = 0; i < fieldValues.length; i++) {
+        let row = fieldValues[i];
+        for (let j = 0; j < row.length; j++) {
+            if(!(row[j].includes('x') || row[j].includes('o'))) {
+                
+            } else {
+                isGameFinished = true;
+            }
+        }
+    }*/
 }
 
 function showEndScreen(winner) {
     document.getElementById('gameField').classList.add('d-none');
     document.getElementById('endScreen-container').innerHTML = generateEndScreen(winner);
+    document.getElementById('endScreen-container').classList.remove('d-none');
+}
+
+function showTieScreen() {
+    document.getElementById('gameField').classList.add('d-none');
+    document.getElementById('endScreen-container').innerHTML = generateTieScreen();
     document.getElementById('endScreen-container').classList.remove('d-none');
 }
 
@@ -118,6 +155,16 @@ function generateEndScreen(winner) {
     
 }
 
+function generateTieScreen() {
+    return `
+    <div id="endScreen" class="endscreen">
+        <span> Unentschieden!</span>
+        <button onclick="restartGame()">noch ein Spiel spielen</button>
+    </div>
+    `;
+    
+}
+
 function restartGame() {
     document.getElementById('gameField').classList.remove('d-none');
     document.getElementById('endScreen-container').classList.add('d-none');
@@ -125,6 +172,7 @@ function restartGame() {
         document.getElementById('value'+i).innerText = "";
     }
     activePlayerIndex = 0;
+    markedFields = 0;
     clearFieldMarkers();
     render();
 }
